9328e58f49d2ebf0fd5ff544b6f7e6d14d0253b0
michalk <mkorbecki@poczta.onet.pl>
2018-02-15 15:49:26 +0100

    Fixed ExpandedAreaWidth.

--- REBUSS.WPF.Controls.HamburgerMenu/HamburgerMenu.cs
+++ REBUSS.WPF.Controls.HamburgerMenu/HamburgerMenu.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace REBUSS.WPF.Controls.HamburgerMenu
 {
@@ -10,16 +12,20 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
             "CollapseMenuTooltip", typeof(string), typeof(HamburgerMenu), new PropertyMetadata(default(string)));
 
         public static readonly DependencyProperty ExpandedAreaWidthProperty = DependencyProperty.Register(
-            "ExpandedAreaWidth", typeof(double), typeof(HamburgerMenu), new PropertyMetadata(250.0));
+            "ExpandedAreaWidth", typeof(double), typeof(HamburgerMenu), new PropertyMetadata(default(double)));
 
         public static readonly DependencyProperty ExpandMenuTooltipProperty = DependencyProperty.Register(
             "ExpandMenuTooltip", typeof(string), typeof(HamburgerMenu), new PropertyMetadata(default(string)));
 
         public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
-            "IsOpen", typeof(bool), typeof(HamburgerMenu), new PropertyMetadata(default(bool)));
+            "IsOpen", typeof(bool), typeof(HamburgerMenu), new PropertyMetadata(default(bool), OnIsOpenChanged));
 
         public static readonly DependencyProperty TextOpacityProperty = DependencyProperty.Register(
             "TextOpacity", typeof(double), typeof(HamburgerMenu), new PropertyMetadata(default(double)));
+
+        private DockPanel itemsControl;
+
+        private Storyboard storyboard;
 
         static HamburgerMenu()
         {
@@ -27,13 +33,17 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
                 new FrameworkPropertyMetadata(typeof(HamburgerMenu)));
         }
 
+        public HamburgerMenu()
+        {
+            Loaded += OnLoaded;
+        }
+
         public string CollapseMenuTooltip
         {
             get { return (string) GetValue(CollapseMenuTooltipProperty); }
             set { SetValue(CollapseMenuTooltipProperty, value); }
         }
-
-        // TODO change storyboard implementation to work with that property
+        
         public double ExpandedAreaWidth
         {
             get { return (double) GetValue(ExpandedAreaWidthProperty); }
@@ -56,6 +66,58 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
         {
             get { return (double) GetValue(TextOpacityProperty); }
             set { SetValue(TextOpacityProperty, value); }
+        }
+
+        private void InitializeExpandAnimation()
+        {
+            storyboard = new Storyboard();
+            storyboard.AutoReverse = false;
+            var widthAnimation = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(widthAnimation, this);
+            Storyboard.SetTargetProperty(widthAnimation, new PropertyPath(WidthProperty));
+            var widthEasing = new EasingDoubleKeyFrame(ExpandedAreaWidth, TimeSpan.FromMilliseconds(300));
+            widthAnimation.KeyFrames.Add(widthEasing);
+            storyboard.Children.Add(widthAnimation);
+            var opacityAnimation = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(opacityAnimation, this);
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(HamburgerMenu.TextOpacityProperty));
+            var opacityEasingA = new EasingDoubleKeyFrame(0.0, TimeSpan.FromMilliseconds(300));
+            var opacityEasingB = new EasingDoubleKeyFrame(1.0, TimeSpan.FromMilliseconds(500));
+            opacityAnimation.KeyFrames.Add(opacityEasingA);
+            opacityAnimation.KeyFrames.Add(opacityEasingB);
+            storyboard.Children.Add(opacityAnimation);
+        }
+
+        private static void OnIsOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var menu = d as HamburgerMenu;
+            if ((bool) e.NewValue)
+            {
+                menu?.StartExpandAnimation();
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            InitializeExpandAnimation();
+            itemsControl = Template.FindName("itemsControl", this) as DockPanel;
+            if (IsOpen)
+            {
+                StartExpandAnimation();
+            }
+        }
+
+        private void StartExpandAnimation()
+        {
+            if (storyboard == null)
+            {
+                InitializeExpandAnimation();
+            }
+
+            if (itemsControl != null)
+            {
+                storyboard?.Begin(itemsControl);
+            }
         }
     }
 }
