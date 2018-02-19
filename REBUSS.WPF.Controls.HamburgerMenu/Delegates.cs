f1635167e1a683d542c188736c8a999b17fe80bb
michalk <mkorbecki@poczta.onet.pl>
2018-02-19 17:03:26 +0100

    Added MenuClosed and MenuOpenedEvent

---
+++ REBUSS.WPF.Controls.HamburgerMenu/Delegates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REBUSS.WPF.Controls.HamburgerMenu
+{
+    public delegate void MenuOpenedEventHandler(object sender, EventArgs args);
+
+    public delegate void PreviewMenuOpenedEventHandler(object sender, EventArgs args);
+
+    public delegate void MenuClosedEventHandler(object sender, EventArgs args);
+
+    public delegate void PreviewMenuClosedEventHandler(object sender, EventArgs args);
+}
--- REBUSS.WPF.Controls.HamburgerMenu/HamburgerMenu.cs
+++ REBUSS.WPF.Controls.HamburgerMenu/HamburgerMenu.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media.Animation;
 
 namespace REBUSS.WPF.Controls.HamburgerMenu
 {
     public class HamburgerMenu : ItemsControl
     {
-        // TODO : Implement 2 routed events: Opened and Closed
         public static readonly DependencyProperty CollapseMenuTooltipProperty = DependencyProperty.Register(
             "CollapseMenuTooltip", typeof(string), typeof(HamburgerMenu), new PropertyMetadata(default(string)));
 
@@ -19,13 +19,23 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
 
         public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
             "IsOpen", typeof(bool), typeof(HamburgerMenu), new PropertyMetadata(default(bool), OnIsOpenChanged));
-
+        
         public static readonly DependencyProperty TextOpacityProperty = DependencyProperty.Register(
             "TextOpacity", typeof(double), typeof(HamburgerMenu), new PropertyMetadata(default(double)));
 
+        public static readonly RoutedEvent MenuClosedEvent =
+            EventManager.RegisterRoutedEvent("MenuClosed", RoutingStrategy.Bubble,
+                typeof(MenuClosedEventHandler), typeof(HamburgerMenu));
+
+        public static readonly RoutedEvent MenuOpenedEvent =
+            EventManager.RegisterRoutedEvent("MenuOpened", RoutingStrategy.Bubble,
+                typeof(MenuOpenedEventHandler), typeof(HamburgerMenu));
+        
         private DockPanel itemsControl;
 
         private Storyboard storyboard;
+
+        private ToggleButton switchButton;
 
         static HamburgerMenu()
         {
@@ -43,7 +53,7 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
             get { return (string) GetValue(CollapseMenuTooltipProperty); }
             set { SetValue(CollapseMenuTooltipProperty, value); }
         }
-        
+
         public double ExpandedAreaWidth
         {
             get { return (double) GetValue(ExpandedAreaWidthProperty); }
@@ -61,13 +71,25 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
             get { return (bool) GetValue(IsOpenProperty); }
             set { SetValue(IsOpenProperty, value); }
         }
-
+        
         public double TextOpacity
         {
             get { return (double) GetValue(TextOpacityProperty); }
             set { SetValue(TextOpacityProperty, value); }
         }
 
+        public event MenuClosedEventHandler MenuClosed
+        {
+            add { AddHandler(MenuClosedEvent, value); }
+            remove { RemoveHandler(MenuClosedEvent, value); }
+        }
+
+        public event MenuOpenedEventHandler MenuOpened
+        {
+            add { AddHandler(MenuOpenedEvent, value); }
+            remove { RemoveHandler(MenuOpenedEvent, value); }
+        }
+        
         private void InitializeExpandAnimation()
         {
             storyboard = new Storyboard();
@@ -80,14 +102,14 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
             storyboard.Children.Add(widthAnimation);
             var opacityAnimation = new DoubleAnimationUsingKeyFrames();
             Storyboard.SetTarget(opacityAnimation, this);
-            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(HamburgerMenu.TextOpacityProperty));
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(TextOpacityProperty));
             var opacityEasingA = new EasingDoubleKeyFrame(0.0, TimeSpan.FromMilliseconds(300));
             var opacityEasingB = new EasingDoubleKeyFrame(1.0, TimeSpan.FromMilliseconds(500));
             opacityAnimation.KeyFrames.Add(opacityEasingA);
             opacityAnimation.KeyFrames.Add(opacityEasingB);
             storyboard.Children.Add(opacityAnimation);
         }
-
+        
         private static void OnIsOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var menu = d as HamburgerMenu;
@@ -101,10 +123,28 @@ namespace REBUSS.WPF.Controls.HamburgerMenu
         {
             InitializeExpandAnimation();
             itemsControl = Template.FindName("itemsControl", this) as DockPanel;
+            switchButton = Template.FindName("switchButton", this) as ToggleButton;
+            
+            if (switchButton != null)
+            {
+                switchButton.Checked += RaiseMenuOpenedEvent;
+                switchButton.Unchecked += RaiseMenuClosedEvent;
+            }
+            
             if (IsOpen)
             {
                 StartExpandAnimation();
             }
+        }
+
+        private void RaiseMenuClosedEvent(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(MenuClosedEvent, this));
+        }
+
+        private void RaiseMenuOpenedEvent(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(MenuOpenedEvent, this));
         }
 
         private void StartExpandAnimation()
